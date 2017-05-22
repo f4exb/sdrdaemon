06711b74c150a908453e61765a6f9a215374e0df
f4exb <f4exb06@gmail.com>
2017-05-22 22:23:15 +0200

    HackRFSink: increased idle carrier level

--- sdmnbase/HackRFSink.cpp
+++ sdmnbase/HackRFSink.cpp
@@ -602,7 +602,7 @@ void HackRFSink::callback(char* buf, int len)
     {
         for (int i = 0; i < len/2; i++)
         {
-            buf[2*i]     = 1;
+            buf[2*i]     = 8;
             buf[2*i+1]   = 0;
         }
     }
